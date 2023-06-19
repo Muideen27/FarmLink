9b413265bc31bf9c82d7cd5669bd70d3d1e08bea
Bucha1958 <okoriemmadu@gmail.com>
2023-06-19 22:18:24 +0100

    Restructuring the data

--- models/buyer.py
+++ models/buyer.py
@@ -4,22 +4,23 @@ import models
 from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy import Column, Integer, String, Enum, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-from models.user import User
 from models.order import Order
 
 Base = declarative_base()
 
-class Buyer(User):
+class Buyer(BaseModel, Base):
     """Buyer class"""
     if models.storage_t == "db":
         __tablename__ = 'buyers'
 
-        id = Column(String(60), ForeignKey('users.id'), primary_key=True)
-        orders = relationship('Order', back_populates='buyer')
+        username = Column(String)
+        email = Column(String)
+        password = Column(String)
+        profile_picture = Column(String)
+        location = Column(String)
+        contact_information = Column(String)
+        orders = relationship('Order', backref='buyer')
 
-        __mapper_args__ = {
-            'polymorphic_identity': 'Buyer'
-        }
--- models/farmer.py
+++ models/farmer.py
@@ -5,17 +5,17 @@ from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
-from models.user import User
 from models.product import Product
 
-class Farmer(User):
+class Farmer(BaseModel, Base):
     if models.storage_t == "db":
         """class farmer"""
         __tablename__ = 'farmers'
 
-        id = Column(String(60), ForeignKey('users.id'), primary_key=True)
-        products = relationship(Product, back_populates='farmer')
-
-        __mapper_args__ = {
-            'polymorphic_identity': 'Farmer'
-        }
+        username = Column(String)
+        email = Column(String)
+        password = Column(String)
+        profile_picture = Column(String)
+        location = Column(String)
+        contact_information = Column(String)
+        products = relationship('product', backref='farmer')
