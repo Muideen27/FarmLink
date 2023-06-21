4aae7a29ae15dc6ae8780a4718997181bd70dd4b
Bucha1958 <okoriemmadu@gmail.com>
2023-06-21 05:33:31 -0700

    update

--- api/v1/views/farmer.py
+++ api/v1/views/farmer.py
@@ -55,4 +55,32 @@ def post_farmers():
         request_body = request.get_json()
     if 'email' not in request_body:
         abort(400, 'Missing email')
-    if 'email' in request_body and 
+    elif 'password' not in request_body:
+        abort(400, 'Missing password')
+    else:
+        email = request_body['email']
+        password = request_body['password']
+        class_name = 'Farmer'  
+
+        # Check if the email already exists for the Farmer class
+        duplicate_email = storage.check_duplicate_email(email, class_name)
+
+        if duplicate_email:
+            abort(400, 'Email address already exists')
+
+        # Create a new Farmer object
+        farmer = Farmer(email=email, password=password)
+
+        # Set other attributes of the farmer object based on request_body
+        if 'username' in request_body:
+            farmer.username = request_body['username']
+        if 'location' in request_body:
+            farmer.location = request_body['location']
+        if 'contact_information' in request_body:
+            farmer.contact_information = request_body['contact_information']
+
+        # Save the farmer object to the database
+        storage.new(farmer)
+        storage.save()
+
+        return jsonify(farmer.to_dict()), 201
